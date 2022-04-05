dc4612a93a04d9457097697afc5843510dca5255
Waleed Saifullah <waleed.saifullah@axiomworld.net>
2022-04-05 21:03:03 +0500

    CRUD Api created

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colors =  require('colors')
+const port = process.env.PORT || 5000
+
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server running on Port ${port}`))
